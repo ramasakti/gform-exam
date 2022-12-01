6a0cb86805b68dde26b37a555acc8142309cb4bb
ramasakti <ramasakti1337@gmail.com>
2022-12-01 14:51:32 +0700

    Optimasi responsif login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <!-- UIkit CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.14/dist/css/uikit.min.css" />
     <!-- UIkit JS -->
@@ -12,28 +15,20 @@
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.15.14/dist/js/uikit-icons.min.js"></script>
 </head>
 <body>
-    
-    <form method="POST" action="/login">
-        @csrf
-        <fieldset class="uk-fieldset uk-position-center">
-            <legend class="uk-legend">Login</legend>
-            @if (session()->has('gagal'))
-                <div class="uk-alert-danger" uk-alert>
-                    <a class="uk-alert-close" uk-close></a>
-                    <p>{{ session('gagal') }}</p>
-                </div>
-            @endif
-
-            <div class="uk-margin">
-                <p class="uk-margin-remove">Username</p>
-                <input class="uk-input uk-form-width-large" name="username" type="text" aria-label="Username">
+    <div class="container uk-margin-large my-3"> 
+        <h4>Login </h4>
+        <form action="/login" method="POST">
+            @csrf			 
+            <div class="mb-3 align-self-center">
+                <label for="exampleInputEmail1" class="form-label">Username</label>
+                <input type="text" name="username" class="form-control" value="" required="">
             </div>
-            <div class="uk-margin">
-                <p class="uk-margin-remove">Password</p>
-                <input class="uk-input uk-form-width-large" name="password" type="password" aria-label="Password">
+            <div class="mb-3 align-self-center">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="password" required="">
             </div>
-            <button type="submit" class="uk-button uk-button-primary uk-width-1-1">LOGIN</button>
-        </fieldset>
-    </form>
+            <button type="submit" class="uk-button uk-button-primary uk-width-1-1">Login</button>
+        </form>
+    </div>
 </body>
 </html>
