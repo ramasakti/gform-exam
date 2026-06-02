9dac71c59141b55581d898d924db4fb21fd0822e
ramasakti <ramasakti1337@gmail.com>
2026-06-02 09:20:58 +0700

    Allow SEB

--- app/Http/Middleware/CheckIframeHeader.php
+++ app/Http/Middleware/CheckIframeHeader.php
@@ -8,7 +8,15 @@ class CheckIframeHeader
 {
     public function handle($request, Closure $next)
     {
-        if ($request->header('X-Requested-With') !== 'com.exambrowser.client' && $request->header('X-Requested-With') !== 'com.cbt.exam.browser' && session('user')->status === 'Siswa') {
+        $userAgent = $request->userAgent();
+        $isSEB = str_contains($userAgent, 'SEB/');
+
+        if (
+            $request->header('X-Requested-With') !== 'com.exambrowser.client' &&
+            $request->header('X-Requested-With') !== 'com.cbt.exam.browser' &&
+            !$isSEB &&
+            session('user')->status === 'Siswa'
+        ) {
             abort(403, 'Forbidden');
         }
 
